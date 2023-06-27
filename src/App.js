4f66697833f53b6aa3046c41ced3c02a7b445cc9
Nguyen Vo <st3phencs@gmail.com>
2023-06-27 20:30:35 +0700

    allroom, add room

--- src/App.js
+++ src/App.js
@@ -1,51 +1,53 @@
-import { Routes, Route } from 'react-router-dom';
-import { ProtectRoute } from '@components/authorization';
-import { role } from '@config';
-import { HomeLayout, UserLayout } from 'layouts';
-import InvoiceRoute from 'routes/invoice-route';
+import { Routes, Route } from "react-router-dom";
+import { ProtectRoute } from "@components/authorization";
+import { role } from "@config";
+import { HomeLayout, UserLayout } from "layouts";
+import InvoiceRoute from "routes/invoice-route";
 
-import RoomRegulationRoute from 'routes/roomRegulation-route';
+import RoomRegulationRoute from "routes/roomRegulation-route";
+import RoomRoute from "routes/room-route";
 
-import RoomPaying from '@pages/search/roomPaying';
-import SearchRoute from 'routes/search-route';
-import Home from '@pages/home/home';
-import ReservationRoute from 'routes/reservation-route';
+import RoomPaying from "@pages/search/roomPaying";
+import SearchRoute from "routes/search-route";
+import Home from "@pages/home/home";
+import ReservationRoute from "routes/reservation-route";
 
-import RoomDetailRoute from 'routes/roomDetail-route';
-import Login from '@pages/auth/login';
-import Notfound from '@pages/error/notfound';
-import ServerInterval from '@pages/error/serverInterval';
-import Unauthorization from '@pages/error/unauthorization';
-import NotFound from '@pages/error/notfound';
+import RoomDetailRoute from "routes/roomDetail-route";
+import Login from "@pages/auth/login";
+import Notfound from "@pages/error/notfound";
+import ServerInterval from "@pages/error/serverInterval";
+import Unauthorization from "@pages/error/unauthorization";
+import NotFound from "@pages/error/notfound";
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route element={<ProtectRoute />}>
-                    <Route path='/hotel' element={<HomeLayout />}>
-                        <Route element={<ProtectRoute allowRoles={[role.MANAGER]} />}>
-                            <Route path='invoice/*' element={<InvoiceRoute />} />
-                        </Route>
-                        <Route path='reservation/*' element={<ReservationRoute />} />
-                        <Route path='room-detail/*' element={<RoomDetailRoute />} />
-                        <Route path='regulation/*' element={<RoomRegulationRoute />} />
-                    </Route>
-                </Route>
+  return (
+    <>
+      <Routes>
+        <Route element={<ProtectRoute />}>
+          <Route path="/hotel" element={<HomeLayout />}>
+            <Route element={<ProtectRoute allowRoles={[role.MANAGER]} />}>
+              <Route path="invoice/*" element={<InvoiceRoute />} />
+            </Route>
+            <Route path="reservation/*" element={<ReservationRoute />} />
+            <Route path="room-detail/*" element={<RoomDetailRoute />} />
+            <Route path="regulation/*" element={<RoomRegulationRoute />} />
+            <Route path="room/*" element={<RoomRoute />} />
+          </Route>
+        </Route>
 
-                <Route path="/" element={<UserLayout />}>
-                    <Route index element={<Home />} />
-                    <Route path='search/*' element={<SearchRoute />} />
-                    <Route path='payment' element={<RoomPaying />} />
-                </Route>
+        <Route path="/" element={<UserLayout />}>
+          <Route index element={<Home />} />
+          <Route path="search/*" element={<SearchRoute />} />
+          <Route path="payment" element={<RoomPaying />} />
+        </Route>
 
-                <Route path="/login" element={<Login />} />
-                <Route path='401' element={<Unauthorization />} />
-                <Route path='404' element={<Notfound />} />
-                <Route path='500' element={<ServerInterval />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </>
-    )
+        <Route path="/login" element={<Login />} />
+        <Route path="401" element={<Unauthorization />} />
+        <Route path="404" element={<Notfound />} />
+        <Route path="500" element={<ServerInterval />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/store/actions/room-action.js
+++ src/store/actions/room-action.js
@@ -1,11 +1,11 @@
 import { room } from "@constant/index";
 
-export const getFreeRooms = payload => ({
-    type: room.GET_FREE_ROOMS,
-    payload
-})
+export const getFreeRooms = (payload) => ({
+  type: room.GET_FREE_ROOMS,
+  payload,
+});
 
-export const getRoomDetail = payload => ({
-    type: room.GET_ROOM_DETAIL,
-    payload
-})
+export const getRoomDetail = (payload) => ({
+  type: room.GET_ROOM_DETAIL,
+  payload,
+});
