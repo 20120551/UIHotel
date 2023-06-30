ecc3d7c6dd8676e7e2abee9c0ba2d7f3b98d1f59
20120551 <killermh1352@gmail.com>
2023-06-30 16:19:57 +0700

    fix staff 401

--- src/services/staff-service.js
+++ src/services/staff-service.js
@@ -1,27 +1,27 @@
-import axios, { privateAxios, createPrivateAxios } from "@lib/axios";
+import axios, { createPrivateAxios } from "@lib/axios";
 
 export const GetAllStaffs = async ({ page, pageSize }) => {
-  const response = await privateAxios.get(
+  const response = await createPrivateAxios().get(
     `/user?page=${page}&pageSize=${pageSize}`
   );
   return response?.data;
 };
 
 export const createStaff = async function (payload) {
-  const response = await privateAxios.post(`/user`, {
+  const response = await createPrivateAxios().post(`/user`, {
     ...payload,
   });
   return response?.data;
 };
 
 export const getStaffInfo = async (id) => {
-  const response = await privateAxios.get(`/user/${id}`);
+  const response = await createPrivateAxios().get(`/user/${id}`);
   return response?.data;
 };
 
 export const changePassWord = async (id, pwd) => {
   console.log(id + " " + pwd);
-  const response = await privateAxios.put(`/user/admin`, {
+  const response = await createPrivateAxios().put(`/user/admin`, {
     id: id,
     newPassword: pwd,
   });
@@ -29,6 +29,6 @@ export const changePassWord = async (id, pwd) => {
 };
 
 export const deleteStaff = async (id) => {
-  const response = await privateAxios.delete(`/user?id=${id}`);
+  const response = await createPrivateAxios().delete(`/user?id=${id}`);
   return response?.data;
 };
